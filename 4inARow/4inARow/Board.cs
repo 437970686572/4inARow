1cfc91becf57e04938374079784c2ca28fea9c33
437970686572 <80xw4ylrb@relay.firefox.com>
2021-11-07 11:28:21 -0800

    Board Class & first Gamelogic

---
+++ 4inARow/4inARow/Board.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4inARow
+{
+    class Board
+    {
+        private List<List<int>> Stones = new();
+
+        public Board(int lenght, int hight)
+        {
+            for (int i = 0; i < hight; i++)
+            {
+                List<int> tmp = new();
+                for (int j = 0; j < lenght; j++)
+                {
+                    tmp.Add(0);
+                }
+                Stones.Add(tmp);
+            }
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+
+            for (int i = 0; i < Stones.Count; i++)
+            {
+                for (int j = 0; j < Stones[i].Count; j++)
+                {
+                    output += Stones[i][j] + "\t";
+                }
+                output += "\n";
+            }
+
+            return output;
+        }
+
+        public bool AddStone(int row, int player)
+        {
+            for (int i = Stones.Count - 1; i > 0; i--)
+            {
+                if(Stones[i][row] == 0)
+                {
+                    Stones[i][row] = player;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool CheckForWin()
+        {
+            return false;
+        }
+    }
+}
--- 4inARow/4inARow/Program.cs
+++ 4inARow/4inARow/Program.cs
@@ -6,7 +6,36 @@ namespace _4inARow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Board board = new(7,6);
+
+            Console.WriteLine(board.ToString());
+
+            Console.ReadLine();
+        }
+
+        private bool DoMove(int player)
+        {
+            return true;
+        }
+
+        private void Game(Board board)
+        {
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                //Do a Move for each player
+                for (int i = 0; i < 2; i++)
+                {
+                    int row;
+                    row = int.Parse(Console.ReadLine());
+                    DoMove(i + 1);
+
+                    if (board.CheckForWin())
+                    {
+                        keepGoing = false;
+                    }
+                }
+            }
         }
     }
 }
